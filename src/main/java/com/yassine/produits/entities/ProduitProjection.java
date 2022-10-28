abf570fcec90c2dd1b0f3b0b3703eb2acfbd3b3d
unknown <mohamedyassinemontacer@gmail.com>
2022-10-28 19:04:30 +0200

    first commit

---
+++ src/main/java/com/yassine/produits/entities/ProduitProjection.java
@@ -0,0 +1,7 @@
+package com.yassine.produits.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+@Projection(name = "nomProd", types = { Produit.class })
+public interface ProduitProjection {
+public String getNomProduit();
+}
